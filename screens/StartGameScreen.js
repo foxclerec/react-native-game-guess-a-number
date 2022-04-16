2f4b01c81138792f12c9180adfbea8a08bdb452e
foxclerec <anatolii.skoryk@gmail.com>
2022-04-16 20:54:40 +0300

    added styles to number input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,7 @@ import PrimaryButton from "../components/PrimaryButton";
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput style={styles.numberInput} maxLength={2} />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -22,5 +22,16 @@ const styles = StyleSheet.create({
     backgroundColor: "#72063c",
     borderRadius: 8,
     elevation: 4,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    textAlign: "center",
+    fontWeight: "bold",
   },
 });
