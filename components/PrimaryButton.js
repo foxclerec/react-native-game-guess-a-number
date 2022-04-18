401c9fd99d00937052f5d2efff146afaffedc7fd
foxclerec <anatolii.skoryk@gmail.com>
2022-04-18 20:34:30 +0300

    added styles to button

--- components/PrimaryButton.js
+++ components/PrimaryButton.js
@@ -1,11 +1,39 @@
-import { View, Text } from "react-native";
+import { StyleSheet, View, Text, Pressable } from "react-native";
 
 const PrimaryButton = ({ children }) => {
+  function pressHandler() {
+    console.log("pressed!");
+  }
+
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={styles.buttonInnerContainer}
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "#fff",
+    textAlign: "center",
+  },
+});
 
 export default PrimaryButton;
